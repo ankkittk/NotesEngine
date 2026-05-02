8421293a3c0b9e02f2e679c35947e42e1f4be0a7
ankkitt <ankittcse@gmail.com>
2026-05-02 22:05:30 +0530

    Implement batch embedding process

--- src/embedder.py
+++ src/embedder.py
@@ -7,8 +7,10 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 def create_embeddings(chunks):
     embeddings = model.encode(
         chunks,
+        batch_size=32,              
         convert_to_numpy=True,
-        normalize_embeddings=True
+        normalize_embeddings=True,
+        show_progress_bar=True
     ).astype("float32")
 
     return embeddings, model
